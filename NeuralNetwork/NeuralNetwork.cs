6a83fc0e82c989eb53efd64c2a4aceed2a8539df
N-Nephilim <valerash2@gmail.com>
2020-02-03 17:03:12 +0200

    Added comments to the code. Now you will understand everything :D (I hope)

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -4,135 +4,152 @@ namespace NeuralNetwork
 {
     class NeuralNetwork
     {
-        public double Moment = 1;
+        public double Moment = 0;
         public double LearningRate = 1;
         public struct Neuron
         {
-            public double value;
-            public double[] w;
+            public double value; // Value of the neuron
+            public double[] w;  //  All weights outgoing from this neuron
         }
         private double[][] previousWeights;
 
         public Neuron[][] network;
 
         #region Network Initialization
-        public NeuralNetwork(string NeuronsAndLayers)
-        {
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+        public NeuralNetwork(string NeuronsAndLayers) //Initializing a neural network
+        { 
             string[] neuronsSTR = NeuronsAndLayers.Split(' ');
-            int[] neurons = new int[neuronsSTR.Length];
-            for (int i = 0; i < neuronsSTR.Length; ++i)
-                neurons[i] = Convert.ToInt32(neuronsSTR[i]);
+            uint[] neurons = new uint[neuronsSTR.Length];
+            for (uint i = 0; i < neuronsSTR.Length; ++i) //Count of neurons in each layer
+                neurons[i] = Convert.ToUInt32(neuronsSTR[i]);
 
             network = new Neuron[neurons.Length][];
             previousWeights = new double[neurons.Length][];
 
-            for (int i = 0; i < neurons.Length; ++i)
+            for (uint i = 0; i < neurons.Length; ++i) //Initializing a Neural Network
                 network[i] = new Neuron[neurons[i]];
 
-            for (int i = 0; i < network.Length - 1; ++i)
+            //Distribution of values in weights of Neural Network and initializing PreviousWeights
+            for (uint i = 0; i < network.Length - 1; ++i) //             Every layer in this NeuralNetwork
             {
                 previousWeights[i] = new double[network[i].Length * network[i + 1].Length];
-                int count = 0;
+                uint countOfWeights = 0;
 
-                for (int j = 0; j < network[i].Length; ++j)
+                for (uint j = 0; j < network[i].Length; ++j) //          Every neuron in layer[i]
                 {
                     Random rand = new Random();
                     network[i][j].w = new double[network[i + 1].Length];
-                    for (int a = 0; a < network[i][j].w.Length; ++a)
+                    for (uint a = 0; a < network[i][j].w.Length; ++a) // Every weight from neuron[i][j]
                     {
-                        previousWeights[i][count] = 0;
-                        ++count;
+                        previousWeights[i][countOfWeights] = 0; //PrewiosWeight on the first iteration = 0
+                        ++countOfWeights;
 
-                    weightWasZero:
-                        network[i][j].w[a] = Math.Round(rand.NextDouble(), 1);
+                    weightWasZeroREPEAT:
+                        network[i][j].w[a] = Math.Round(rand.NextDouble(), 1); //Random value for the weight
                         if (network[i][j].w[a] == 0)
-                            goto weightWasZero;
+                            goto weightWasZeroREPEAT; //Value of weight cannot be 0
                         // Console.WriteLine("Weigth " + (i + 1) + " " + (j+1) + " = " + network[i][j].w[a]);
                     }
                 }
             }
         }
+//-------------------------------------------------------------------------------------------------------------------------------------------------
         #endregion
 
         #region NeuralNetwork Run
-        private static double Sigmoid(double num)
-        {
-            return (1.0 / (1 + Math.Pow(Math.E, -num)));
-        }
+//-------------------------------------------------------------------------------------------------------------------------------------------------
         public Neuron[] RunNetwork(double[] training)
         {
-            for(int i = 0; i < training.Length; ++i)
-                network[0][i].value = training[i];
+            //INPUT assignment
+            for (uint j = 0; j < training.Length; ++j)
+                network[0][j].value = training[j];
 
-            for (int i = 1; i < network.Length; ++i)
+            double output;
+            for (uint i = 1; i < network.Length; ++i) //                Every layer in this NeuralNetwork
             {
-                for (int j = 0; j < network[i].Length; ++j)
+                for (uint j = 0; j < network[i].Length; ++j) //         Every neuron in layer[i]
                 {
-                    double output = 0;
-                    for (int k = 0; k < network[i - 1].Length; ++k)
+                    //Calculation of value of the neuron, depending on all values of neurons and weights of synapses connected to this neuron
+                    output = 0;
+                    for (uint k = 0; k < network[i - 1].Length; ++k) // Summ of every neuron in layer[i-1] * every neuron of neuron[i-1] and it's weight[j]
                         output += network[i - 1][k].value * network[i - 1][k].w[j];
 
+                    //Value activation 
                     network[i][j].value = Sigmoid(output);
                 }
             }
 
             return network[network.GetLength(0) - 1];
-        }
+        }  //Function to Run the network
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+        private static double Sigmoid(double num)
+        {
+            return (1.0 / (1 + Math.Pow(Math.E, -num)));
+        } //Activation function for RUN
         #endregion
 
-        #region NeuralNetwork Teach
-        private static double SigmoidError(double output)
+        #region NeuralNetwork Train
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+        public void TeachNetwork(double[] ideal, Neuron[] output) //Function to Train the network
         {
-            return ((1 - output) * output);
-        }
-        private static double DeltaOut(double outIdeal, double outActual)
-        {
-            return ((outIdeal - outActual) * SigmoidError(outActual));
-        }
-        private static double DeltaHidden(double[] weights, double output, Neuron[] delta)
-        {
-            double actualSum = 0;
-
-            for (int i = 0; i < weights.Length; ++i)
-                actualSum += weights[i] * delta[i].value;
-
-            return (SigmoidError(output) * actualSum);
-        }
-        private static double GRAD(double output, double delta)
-        {
-            return (output * delta);
-        }
-        private double deltaW(double Grad, double prWeight)
-        {
-            return (LearningRate * Grad + Moment * prWeight);
-        }
-        public void TeachNetwork(double[] ideal, Neuron[] output)
-        {
+            //Creating a copy of NeuralNetwork to work with it
             Neuron[][] deltaNetwork = new Neuron[network.Length][];
-            for (int i = 0; i < network.Length; ++i)
+            for (uint i = 0; i < network.Length; ++i)
             {
                 deltaNetwork[i] = new Neuron[network[i].Length];
-                for (int j = 0; j < network[i].Length; ++j)
+                for (uint j = 0; j < network[i].Length; ++j)
                     deltaNetwork[i][j] = network[i][j];
             }
 
-            for(int i = 0; i < ideal.Length; ++i)
+            //Calculating Delta(OUT) for OUTPUT neurons of the NeuralNetwork 
+            for (uint i = 0; i < ideal.Length; ++i)
                 deltaNetwork[deltaNetwork.Length - 1][i].value = DeltaOut(ideal[i], output[i].value);
 
-            for (int i = deltaNetwork.Length - 2; i >= 1; --i)
-                for (int j = 0; j < network[i].Length; ++j)
-                    deltaNetwork[i][j].value = DeltaHidden(deltaNetwork[i][j].w, network[i][j].value, deltaNetwork[i + 1]);
+            //Calculating Delta(HIDDEN) for HIDDEN neurons the NeuralNetwork 
+            for (int i = deltaNetwork.Length - 2; i >= 1; --i) //Start - from the last HIDDEN layer | End - to the firs HIDDEN layer
+                for (uint j = 0; j < network[i].Length; ++j)
+                    deltaNetwork[i][j].value = DeltaHidden(deltaNetwork[i][j].w, deltaNetwork[i + 1], network[i][j].value);
 
-            for (int i = deltaNetwork.Length - 2; i >= 0; --i)
-                for (int j = 0; j < network[i].Length; ++j)
-                    for (int a = 0; a < network[i][j].w.Length; ++a)
+            //Calculating delta of all the weights to change them
+            double Grad;
+            double delta;
+            for (int i = deltaNetwork.Length - 2; i >= 0; --i) //        Start - from the last HIDDEN layer | End - to the firs layer (INPUT)
+                for (uint j = 0; j < network[i].Length; ++j) //          Every neuron in layer[i]
+                    for (uint a = 0; a < network[i][j].w.Length; ++a) // Every weight from neuron[i][j]
                     {
-                        double Grad = GRAD(network[i][j].value, deltaNetwork[i + 1][a].value);
-                        double delta = deltaW(Grad, previousWeights[i][j]);
-                        network[i][j].w[a] += delta;
+                        Grad = GRAD(network[i][j].value, deltaNetwork[i + 1][a].value);
+                        delta = deltaW(Grad, previousWeights[i][j]);
+                        network[i][j].w[a] += delta; //Change the weight of synapse (ActualWeight + DeltaOfThisWeight)
                         previousWeights[i][j] = delta;
                     }
         }
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+        private static double SigmoidError(double output)
+        {
+            return ((1 - output) * output);
+        } //Activation function for TRAIN
+        private static double DeltaOut(double outIdeal, double outActual)
+        {
+            return ((outIdeal - outActual) * SigmoidError(outActual));
+        } //Calculation delta of OUT neuron
+        private static double DeltaHidden(double[] weights, Neuron[] delta, double output)
+        {
+            double actualSum = 0;
+
+            for (uint i = 0; i < weights.Length; ++i) //((w1 * d1) + (w2 * d2) + .. + (wn * dn))
+                actualSum += weights[i] * delta[i].value;
+
+            return (SigmoidError(output) * actualSum);
+        } //Calculation delta of HIDDEN neuron
+        private static double GRAD(double output, double delta)
+        {
+            return (output * delta);
+        } //Calculation gradient(gradient descent) for the weight
+        private double deltaW(double Grad, double prWeight)
+        {
+            return (LearningRate * Grad + Moment * prWeight);
+        } //Calculation delta of the weight
         #endregion
 
     }
--- NeuralNetwork/Program.cs
+++ NeuralNetwork/Program.cs
@@ -6,120 +6,129 @@ namespace NeuralNetwork
 {
     class NeuralNetworkPROBuilderXOR
     {
-        const double Moment = 0.1;
+        const double Moment = 0.01;
         const double LearningRate = 0.1;
-        const string NeuronsAndLayers = "8 4 2 1";
+        const string NeuronsAndLayers = "8 4 2 1"; //"[0]-InputNeurons, [1]-Neurons In 1-st HiddenLayer,
+                                                  //  [2]-Neurons In 2-nd HiddenLayer,[..],[n-1]-Neurons In (n-1)-th HiddenLayer, [n]-OutputNeurons"
         public struct TrainAndTest
         {
-            public double[] IN;
-            public double[] OUT;
+            public double[] IN;   //All Input values 
+            public double[] OUT; // All output values
         }
 
         static void Main()
         {
-            #region Training&Set
+            #region Training&Test initialization
             string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\numbers.txt");
             string[] lines = File.ReadAllLines(filePath);
 
-            int trainingCount = lines.Length;
+            //TRAIN units
+            TrainAndTest[] trainingData = new TrainAndTest[lines.Length / 2];
+            //Training and test files are .txt, where non-odd lines are INPUT values and odd lines are OUTPUT values
 
-            TrainAndTest[] trainingData = new TrainAndTest[trainingCount/2];
-
-            int count = 0;
-            for(int i = 1; i <= lines.Length; ++i)
-            {
-                string[] line = lines[i-1].Split(' ');
-                if (i % 2 != 0)
-                {
-                    trainingData[count].IN = new double[line.Length];
-                    for (int j = 0; j < line.Length; ++j)
-                        trainingData[count].IN[j] = Convert.ToDouble(line[j]);
-                }
-                else
-                {
-                    trainingData[count].OUT = new double[line.Length];
-                    for (int j = 0; j < line.Length; ++j)
-                        trainingData[count].OUT[j] = Convert.ToDouble(line[j]);
-                    ++count;
-                }
-            }
-
-            filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\numbersTEST.txt");
-            lines = File.ReadAllLines(filePath);
-
-            int testCount = lines.Length;
-            TrainAndTest[] testData = new TrainAndTest[testCount/2];
-
-            count = 0;
-            for (int i = 1; i <= lines.Length; ++i)
+            uint countOfUnit = 0;
+            for (uint i = 1; i <= lines.Length; ++i)
             {
                 string[] line = lines[i - 1].Split(' ');
                 if (i % 2 != 0)
                 {
-                    testData[count].IN = new double[line.Length];
-                    for (int j = 0; j < line.Length; ++j)
-                        testData[count].IN[j] = Convert.ToDouble(line[j]);
+                    trainingData[countOfUnit].IN = new double[line.Length];
+                    for (uint j = 0; j < line.Length; ++j) //Every INPUT value in the line must be readen
+                        trainingData[countOfUnit].IN[j] = Convert.ToDouble(line[j]);
                 }
                 else
                 {
-                    testData[count].OUT = new double[line.Length];
-                    for (int j = 0; j < line.Length; ++j)
-                        testData[count].OUT[j] = Convert.ToDouble(line[j]);
-                    ++count;
+                    trainingData[countOfUnit].OUT = new double[line.Length];
+                    for (uint j = 0; j < line.Length; ++j) //Every OUTPUT value in the line must be readen
+                        trainingData[countOfUnit].OUT[j] = Convert.ToDouble(line[j]);
+                    ++countOfUnit;
+                }
+            }
+
+            //The same for TEST units
+            filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\numbersTEST.txt");
+            lines = File.ReadAllLines(filePath);
+
+            TrainAndTest[] testData = new TrainAndTest[lines.Length / 2];
+
+            countOfUnit = 0;
+            for (uint i = 1; i <= lines.Length; ++i)
+            {
+                string[] line = lines[i - 1].Split(' ');
+                if (i % 2 != 0)
+                {
+                    testData[countOfUnit].IN = new double[line.Length];
+                    for (uint j = 0; j < line.Length; ++j)
+                        testData[countOfUnit].IN[j] = Convert.ToDouble(line[j]);
+                }
+                else
+                {
+                    testData[countOfUnit].OUT = new double[line.Length];
+                    for (uint j = 0; j < line.Length; ++j)
+                        testData[countOfUnit].OUT[j] = Convert.ToDouble(line[j]);
+                    ++countOfUnit;
                 }
             }
             #endregion
 
-            #region Main part - network teaching
-            double terminatingError = 0.00001;
-            uint refreshSpeed = 100000;
+            #region Main part - network training
+            double terminatingErrorProcents = 0.001; //The average error procent on which we want to end training
+            uint refreshSpeed = 10000;
 
+            //Creating an object of NeuralNetwork with same parameters as we described in variables
             NeuralNetwork network = new NeuralNetwork(NeuronsAndLayers);
             network.Moment = Moment;
             network.LearningRate = LearningRate;
 
-
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
             double errorSum;
             uint iteration = 0;
+            double end;
+            double error;
             do
             {
                 errorSum = 0;
-                for (int i = 0; i < trainingData.Length; ++i)
+                for (uint i = 0; i < trainingData.Length; ++i) //Run through all TRAIN units
                 {
+                    //Running the network with current INPUT values of this unit
                     NeuralNetwork.Neuron[] endValue = network.RunNetwork(trainingData[i].IN);
 
-                    double end = 0;
-                    for(int j = 0; j < endValue.Length; ++j)
+                    //Counting an error of current unit
+                    end = 0;
+                    for (uint j = 0; j < endValue.Length; ++j)
                         end += Math.Pow(trainingData[i].OUT[j] - endValue[j].value, 2);
-                    
-                    double error = end / trainingData.Length; // ((i1-a1)*(i1-a1)+...+(in-an)*(in-an))/n
+                    error = end / trainingData.Length; //((i-a1)*(i1-a1)+...+(in-an)*(in-an))/n
                     errorSum += error;
 
                     network.TeachNetwork(trainingData[i].OUT, endValue);
                 }
                 if (iteration++ % refreshSpeed == 0)
-                    Console.WriteLine(iteration + " summ of errors = " + Math.Round((errorSum / trainingData.GetLength(0)) * 100, 5) + "%");
+                    Console.WriteLine(iteration + " average error = " + Math.Round((errorSum / trainingData.GetLength(0)) * 100, 5) + "%");
 
-            } while ((errorSum / trainingData.GetLength(0)) > terminatingError);
+            } while ((errorSum / trainingData.GetLength(0)) * 100 > terminatingErrorProcents); //while average error procent is greater tnah TEP - continue
+            sw.Stop();
             #endregion
 
             #region Output
-            int errCount = 0;
-            for (int i = 0; i < testData.GetLength(0); ++i)
+            uint errCount = 0;
+            for (uint i = 0; i < testData.GetLength(0); ++i) //Run through all TEST units
             {
                 NeuralNetwork.Neuron[] answer = network.RunNetwork(testData[i].IN);
                 double[] _answer = new double[answer.Length];
 
-                for (int j = 0; j < answer.Length; ++j)
+                for (uint j = 0; j < answer.Length; ++j) //Normalizing answers in this unit
                     _answer[j] = answer[j].value > 0.5 ? 1 : 0;
-                for (int j = 0; j < answer.Length; ++j)
+
+                for (uint j = 0; j < answer.Length; ++j) //Checking answers in this unit for a mistake
                 {
-                    if (_answer[j] != testData[i].OUT[j])
+                    if (_answer[j] != testData[i].OUT[j]) //If a mistake was made then:
                     {
-                        for (int a = 0; a < testData[i].IN.Length; ++a)
+                        for (uint a = 0; a < testData[i].IN.Length; ++a) // -Write down all INPUTs 
                             Console.Write($"I{a}={testData[i].IN[a]} ");
                         Console.WriteLine();
-                        Console.WriteLine("Real answer = " + answer[j].value + " Activated answer = " + _answer[j] + " Ideal = " + testData[i].OUT[j]);
+                        for (uint k = 0; k < answer.Length; ++k) // -Write down all OUTPUTS + activated OUTPUTS + ideal OUTPUTS
+                            Console.WriteLine("Real answer = " + answer[k].value + " Activated answer = " + _answer[k] + " Ideal = " + testData[i].OUT[k]);
                         ++errCount;
                         Console.WriteLine();
                     }
@@ -128,7 +137,7 @@ namespace NeuralNetwork
 
             Console.WriteLine("\nError count = " + errCount);
             Console.WriteLine("\nNumber of iterations = " + iteration);
-
+            Console.WriteLine("Training time = " + ((double)sw.ElapsedMilliseconds/1000).ToString() + " sec");
             #endregion
         }
     }
