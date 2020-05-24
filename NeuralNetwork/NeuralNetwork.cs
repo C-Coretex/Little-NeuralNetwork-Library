d5fe8469698e35646219ef6aece42291a83e9105
diademoff <diademoff@yandex.ru>
2020-05-25 01:50:10 +0300

    overload TeachNetwork

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -5,36 +5,30 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace NN
 {
-    //---------------------------------------------------------------
-    //
-    //
-    //  This file is a library with public methods to use Neural Network.
-    //  Check another file to understand how to use the library.
-    //  You are free to use the library in yout own needs
-    //
-    //
-    //---------------------------------------------------------------
+    [Serializable]
+    public struct Neuron // TODO: separate in to file
+    {
+        public double value;
+        public double[] weights;
+    }
+    /// <summary>
+    /// This file is a library with public methods to use Neural Network.
+    /// Check another file to understand how to use the library.
+    /// You are free to use the library in yout own needs
+    /// </summary>
     [Serializable]
     public class NeuralNetwork //output  =  sum (weights * inputs) + bias 
     {
-        public double Moment { get; set; } = 0;
-        public double LearningRate { get; set; } = 1;
-
-        [Serializable]
-        /// <summary>
-        /// The struct 'Neuron[]'. Has double 'value' - value of the neuron and double[] 'w' - weights outhoing from this neuron
-        /// </summary>
-        public struct Neuron
-        {
-            public double value; // Value of the neuron
-            public double[] w;  //  All weights outgoing from this neuron
-        }
         /// <summary>
         /// The public struct(Neuron[][]) network. To access to the certain neuron type: network[i][j]
         /// </summary>
-        public Neuron[][] network { get; set; }
-        private Neuron[][] deltaNetwork;
+        public Neuron[][] Network { get; set; }
+        public double Moment { get; set; } = 0;
+        public double LearningRate { get; set; } = 1;
+        public int LayersCount => Network.Length;
 
+
+        private Neuron[][] deltaNetwork;
         private double[][] previousWeights;
         private uint[] withoutBiasLength;
 
@@ -43,22 +37,22 @@ namespace NN
         private int i;
         private int k;
         private int a;
+        Random rand = new Random();
         double output;
         double Grad;
         double delta;
 
-        #region Network Initialization/Load/Save
-        //-------------------------------------------------------------------------------------------------------------------------------------------------
+
         /// <summary>
         /// Creates the network.
         /// </summary>
-        /// <param name="NeuronsAndLayers">The struct of the network. For instance, "4+ 15+ 5 3" whare '+' means bias.</param>
+        /// <param name="NeuronsAndLayers">The struct of the network. For instance, "4+ 15+ 5 3" whare '+' means bias. Watch readme</param>
         /// <param name="randMin">Minimal random weight of synapse.</param>
         /// <param name="randMax">Maximal random weight of synapse.</param>
-        public NeuralNetwork(string NeuronsAndLayers, double randMin, double randMax) //Initializing a neural network
+        public NeuralNetwork(string NeuronsAndLayers, double randMin, double randMax)
         { 
             string[] neuronsSTR = NeuronsAndLayers?.Split(' ');
-            network = new Neuron[neuronsSTR.Length][];
+            Network = new Neuron[neuronsSTR.Length][];
             withoutBiasLength = new uint[neuronsSTR.Length];
             ArrayList biases = new ArrayList();
             for (i = 0; i < neuronsSTR.Length; ++i) //Count of neurons in each layer
@@ -66,7 +60,7 @@ namespace NN
                 try //Check if there are biases
                 {
                     uint index = Convert.ToUInt32(neuronsSTR[i]);
-                    network[i] = new Neuron[index];
+                    Network[i] = new Neuron[index];
                     withoutBiasLength[i] = index;
                 }
                 catch (Exception) //If bias is in the layer then
@@ -77,82 +71,88 @@ namespace NN
                     {
                         biases.Add(i);
                         uint index = Convert.ToUInt32(neuronsSTR[i][0..^1]) + 1;
-                        network[i] = new Neuron[index]; //Convert only count of neurons without bias(+)
-                        network[i][index - 1].value = 1;
+                        Network[i] = new Neuron[index]; //Convert only count of neurons without bias(+)
+                        Network[i][index - 1].value = 1;
                         withoutBiasLength[i] = index - 1;
                     }
                 }
             }
 
-            previousWeights = new double[network.Length][];
+            previousWeights = new double[Network.Length][];
             //Distribution of values in weights of Neural Network and initializing PreviousWeights
-            for (i = 0; i < network.Length - 1; ++i) //             Every layer in this NeuralNetwork
+            for (i = 0; i < Network.Length - 1; ++i) //             Every layer in this NeuralNetwork
             {
-                previousWeights[i] = new double[network[i].Length * withoutBiasLength[i + 1]];
+                previousWeights[i] = new double[Network[i].Length * withoutBiasLength[i + 1]];
                 uint countOfWeights = 0;
 
-                for (j = 0; j < network[i].Length; ++j) //          Every neuron in layer[i]
+                for (j = 0; j < Network[i].Length; ++j) //          Every neuron in layer[i]
                 {
-                    Random rand = new Random();
-                    network[i][j].w = new double[withoutBiasLength[i + 1]];
-                    for (uint a = 0; a < network[i][j].w.Length; ++a) // Every weight from neuron[i][j]
+                    Network[i][j].weights = new double[withoutBiasLength[i + 1]];
+                    for (uint a = 0; a < Network[i][j].weights.Length; ++a) // Every weight from neuron[i][j]
                     {
                         previousWeights[i][countOfWeights] = 0; //PrewiosWeight on the first iteration = 0
                         ++countOfWeights;
 
                     weightWasZeroREPEAT:
-                        network[i][j].w[a] = Math.Round(rand.NextDouble() * (randMax - randMin) + randMin, 5); //Random value for the weight
-                        if (network[i][j].w[a] == 0)
+                        Network[i][j].weights[a] = Math.Round(rand.NextDouble() * (randMax - randMin) + randMin, 5); //Random value for the weight
+                        if (Network[i][j].weights[a] == 0)
                             goto weightWasZeroREPEAT; //Value of weight cannot be 0
                     }
                 }
             }
 
             //Creating a copy of NeuralNetwork to work with it
-            deltaNetwork = new Neuron[network.Length][];
-            for (i = 0; i < network.Length; ++i)
+            deltaNetwork = new Neuron[Network.Length][];
+            for (i = 0; i < Network.Length; ++i)
             {
-                deltaNetwork[i] = new Neuron[network[i].Length];
-                for (j = 0; j < network[i].Length; ++j)
-                    deltaNetwork[i][j] = network[i][j];
+                deltaNetwork[i] = new Neuron[Network[i].Length];
+                for (j = 0; j < Network[i].Length; ++j)
+                    deltaNetwork[i][j] = Network[i][j];
             }
         }
         /// <summary>
         /// Loads the network.
         /// </summary>
         /// <param name="pathAndName">The path to the file with its name.</param>
-        public NeuralNetwork(string pathAndName) //Loading a neural network
+        public NeuralNetwork(string pathAndName)
         {
             if (File.Exists(pathAndName))
             {
-                using (FileStream fs = new FileStream(pathAndName, FileMode.Open))
+                try
                 {
-                    BinaryFormatter formatter = new BinaryFormatter();
-                    NeuralNetwork n = (NeuralNetwork)formatter.Deserialize(fs);
-                    network = n.network;
-                    previousWeights = n.previousWeights;
-                    Moment = n.Moment;
-                    LearningRate = n.LearningRate;
+                    using (FileStream fs = new FileStream(pathAndName, FileMode.Open))
+                    {
+                        BinaryFormatter formatter = new BinaryFormatter();
+                        NeuralNetwork n = (NeuralNetwork)formatter.Deserialize(fs);
+                        Network = n.Network;
+                        previousWeights = n.previousWeights;
+                        Moment = n.Moment;
+                        LearningRate = n.LearningRate;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    throw new IOException($"Couldn't open file {pathAndName}\r\n{ex.ToString()}");
                 }
 
                 //Creating a copy of NeuralNetwork to work with it
-                deltaNetwork = new Neuron[network.Length][];
-                for (i = 0; i < network.Length; ++i)
+                deltaNetwork = new Neuron[Network.Length][];
+                for (i = 0; i < Network.Length; ++i)
                 {
-                    deltaNetwork[i] = new Neuron[network[i].Length];
-                    for (j = 0; j < network[i].Length; ++j)
-                        deltaNetwork[i][j] = network[i][j];
+                    deltaNetwork[i] = new Neuron[Network[i].Length];
+                    for (j = 0; j < Network[i].Length; ++j)
+                        deltaNetwork[i][j] = Network[i][j];
                 }
             }
             else
-                throw new ArgumentException("This file does not exist.\rTry to recheck the path or just save new Neural Network with <SaveNetwork> method", "NeuralNetwork");
+                throw new FileNotFoundException("This file does not exist.\rTry to recheck the path or just save new Neural Network with <SaveNetwork> method", pathAndName);
         }
 
         /// <summary>
         /// Saves the network.
         /// </summary>
         /// <param name="pathAndName">The path to the file with its name.</param>
-        public void SaveNetwork(string pathAndName) //Saving a neural network
+        public void SaveNetwork(string pathAndName)
         {
             using (FileStream fs = new FileStream(pathAndName, FileMode.Create))
             {
@@ -160,84 +160,88 @@ namespace NN
                 formatter.Serialize(fs, this);
             }
         }
-        //-------------------------------------------------------------------------------------------------------------------------------------------------
-        #endregion
 
-        #region NeuralNetwork Run
-        //-------------------------------------------------------------------------------------------------------------------------------------------------
         /// <summary>
         /// Runs the network and returns Output neurons with the struct Neuron[].
         /// </summary>
-        /// <param name="training">This is array that contains ont training set(iteration).</param>
+        /// <param name="inputValues">This is array that contains input set.</param>
         /// <returns>Neuron[]</returns>
-        public virtual Neuron[] RunNetwork(double[] training) //Function to Run the network
+        public virtual Neuron[] RunNetwork(double[] inputValues)
         {
-            //INPUT assignment
-            for (j = 0; j < training.Length; ++j)
-                network[0][j].value = training[j];
+            //INPUT neurons assignment
+            for (j = 0; j < inputValues.Length; ++j)
+                Network[0][j].value = inputValues[j];
 
-            for (i = 1; i < network.Length; ++i) //                Every layer in this NeuralNetwork
+            for (i = 1; i < LayersCount; ++i)
             {
-                for (j = 0; j < withoutBiasLength[i]; ++j) //         Every neuron in layer[i]
+                for (j = 0; j < withoutBiasLength[i]; ++j) // Every neuron in layer[i]
                 {
                     // Calculation value of the neuron, depending on all values of neurons and weights of synapses connected to this neuron
                     output = 0;
-                    for (k = 0; k < network[i - 1].Length; ++k) // Summ of every neuron in layer[i-1] * every neuron of weight[i-1] = weight[j]
-                        output += network[i - 1][k].value * network[i - 1][k].w[j];
+                    for (k = 0; k < Network[i - 1].Length; ++k) // Summ of every neuron in layer[i-1] * every neuron of weight[i-1] = weight[j]
+                        output += Network[i - 1][k].value * Network[i - 1][k].weights[j];
 
                     //Value activation 
-                    network[i][j].value = Sigmoid(output);
+                    Network[i][j].value = Sigmoid(output);
                 }
             }
 
-            return network[network.GetLength(0) - 1];
+            return Network[Network.GetLength(0) - 1];
         }
-//-------------------------------------------------------------------------------------------------------------------------------------------------
-        private double Sigmoid(double num)
-        {
-            return (1.0 / (1 + Math.Pow(Math.E, -num)));
-        } //Activation function for RUN
-        #endregion
 
-        #region NeuralNetwork Train
-        //-------------------------------------------------------------------------------------------------------------------------------------------------
         /// <summary>
-        /// Teaches the network.
+        /// Function to Train the network after run
         /// </summary>
         /// <param name="ideal">The expected values(double[]).</param>
-        /// <param name="output">The actual values(Neuron[]).</param>
-        ///
-        public virtual void TeachNetwork(double[] ideal, Neuron[] output) //Function to Train the network
+        public virtual void TeachNetwork(double[] ideal)
         {
             //Calculating Delta(OUT) for OUTPUT neurons of the NeuralNetwork 
             for (i = 0; i < ideal.Length; ++i)
-                 deltaNetwork[^1][i].value = DeltaOut(ideal[i], output[i].value);
+                 deltaNetwork[^1][i].value = DeltaOut(ideal[i], Network[^1][i].value);
 
             //Calculating Delta(HIDDEN) for HIDDEN neurons the NeuralNetwork 
             for (i = deltaNetwork.Length - 2; i >= 1; --i) //Start - from the last HIDDEN layer | End - to the firs HIDDEN layer
-                for (j = 0; j < network[i].Length; ++j)
-                    deltaNetwork[i][j].value = DeltaHidden(network[i][j].w, deltaNetwork[i + 1], network[i][j].value);
+                for (j = 0; j < Network[i].Length; ++j)
+                    deltaNetwork[i][j].value = DeltaHidden(Network[i][j].weights, deltaNetwork[i + 1], Network[i][j].value);
 
             //Calculating delta of all the weights to change them
             for (i = deltaNetwork.Length - 2; i >= 0; --i) //        Start - from the last HIDDEN layer | End - to the firs layer (INPUT)
-                for (j = 0; j < network[i].Length; ++j) //          Every neuron in layer[i]
-                    for (a = 0; a < network[i][j].w.Length; ++a) // Every weight from neuron[i][j]
+                for (j = 0; j < Network[i].Length; ++j) //          Every neuron in layer[i]
+                    for (a = 0; a < Network[i][j].weights.Length; ++a) // Every weight from neuron[i][j]
                     {
-                        Grad = network[i][j].value * deltaNetwork[i + 1][a].value; //Calculating gradient(gradient descent) for the weight
+                        Grad = Network[i][j].value * deltaNetwork[i + 1][a].value; //Calculating gradient(gradient descent) for the weight
                         delta = LearningRate*Grad + Moment*previousWeights[i][j]; //Calculating delta of the weight
-                        network[i][j].w[a] += delta; //Change the weight of synapse (ActualWeight + DeltaOfThisWeight)
+                        Network[i][j].weights[a] += delta; //Change the weight of synapse (ActualWeight + DeltaOfThisWeight)
                         previousWeights[i][j] = delta;
                     }
         }
-        //-------------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Function to Train the network.
+        /// </summary>
+        /// <param name="input"> Values for INPUT neurons </param>
+        /// <param name="ideal"> Values for OUTPUT neurons </param>
+        public virtual void TeachNetwork(double[] input, double[] ideal)
+        {
+            RunNetwork(input);
+            TeachNetwork(ideal);
+        }
+
+        private double Sigmoid(double num)
+        {
+            return (1.0 / (1 + Math.Pow(Math.E, -num)));
+        }
+
         private double SigmoidError(double output)
         {
-            return ((1 - output) * output);
-        } //Activation function for TRAIN
+            return (1 - output) * output;
+        }
+
         private double DeltaOut(double outIdeal, double outActual)
         {
             return ((outIdeal - outActual) * SigmoidError(outActual));
-        } //Calculation delta of OUT neuron
+        }
+
         private double DeltaHidden(double[] weights, Neuron[] delta, double output)
         {
             double actualSum = 0;
@@ -246,8 +250,6 @@ namespace NN
                 actualSum += weights[k] * delta[k].value;
 
             return (SigmoidError(output) * actualSum);
-        } //Calculation delta of HIDDEN neuron
-        #endregion
-
+        }
     }
 }
